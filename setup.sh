0029b695df2e769cf1d04590d0094110aec840a5
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-10-02 22:11:44 +0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo
+echo "Tunggu Sampai Proses Selesai Ya Anjing, Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
+pkg install python
+pkg install ruby -y
+clear
+gem install lolcat
+clear
+pkg install figlet
+clear
+figlet YayanXD | lolcat
+clear
+pip2 install -r requirements.txt
+clear
+pip2 install mechanize
+clear
+pip2 install requests
