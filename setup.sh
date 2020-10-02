d0539e4a6ed892426b5c67675da40d9415d07d12
Moch Yayan Juan Alvredo XD <69158355+Yayan-XD@users.noreply.github.com>
2020-10-03 05:06:30 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,19 +2,30 @@
 #Bakekok bunda xixixi:v
 #Mau ngapain om? gada apa2 disini
 clear
+echo
+echo "Tunggu Sampai Proses Selesai Tod Sabar..!"
+echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v"
+echo
 pkg install python
-clear
 pkg install ruby -y
 clear
 gem install lolcat
-echo
-echo "Tunggu Sampai Proses Selesai Ya Anjing, Sabar..!" | lolcat
-echo "Ucapkan Yayan Ganteng 3X Biar Work Xixixi:v" |lolcat
-echo
+clear
 pkg install figlet
 clear
 figlet YayanXD | lolcat
 clear
+figlet YayanXD | lolcat
+clear
+figlet YayanXD | lolcat
+echo
+echo "╭╮╮╱▔▔▔▔╲╭╭╮" | lolcat
+echo "╰╲╲▏▂╲╱▂▕╱╱╯" | lolcat
+echo "┈┈╲▏▇▏▕▇▕╱┈┈" | lolcat
+echo "┈┈╱╲▔▕▍▔╱╲┈┈" | lolcat
+echo "╭╱╱▕╋╋╋╋▏╲╲╮" | lolcat
+echo "╰╯╯┈╲▂▂╱┈╰╰╯" | lolcat
+echo
 pip2 install -r requirements.txt
 clear
 pip2 install mechanize
